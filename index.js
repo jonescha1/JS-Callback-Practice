c89136a536fa775b63dc4e37289bad7b4d2947f9
Chase Jones <chasejones@Chases-MacBook-Pro.local>
2022-05-10 19:25:16 -0400

    First Commit

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+const character = newImage("assets/green-character/static.gif");
 
+handleDirectionChange = (direction) => {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
+};
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
+
+document.addEventListener("keydown", function (e) {
+  if (e.repeat) return;
+
+  if (e.key === "ArrowLeft") {
+    direction = "west";
+  }
+  if (e.key === "ArrowUp") {
+    direction = "north";
+  }
+  if (e.key === "ArrowRight") {
+    direction = "east";
+  }
+  if (e.key === "ArrowDown") {
+    direction = "south";
+  }
+});
+
+document.addEventListener("keyup", function (e) {
+  direction = null;
+});
